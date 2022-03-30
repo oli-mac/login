7fe871adea138f9d6022d44c59d4a0fa542c53a5
oli-mac <adumulugeta21@gmail.com>
2022-03-31 00:36:24 +0300

    half way done

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,4 +6,10 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name="home"),
+    path('signup',views.signup, name="signup"),
+    path('signin',views.signin, name="signin"),
+    path('signout',views.signout, name="signout"),
+
+
+
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,59 @@
 from http.client import HTTPResponse
-from django.shortcuts import render
+from wsgiref.util import request_uri
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+
+
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("hello bitch ass")
+    return render(request, "authentication/index.html")
+
+def signup(request):
+    if request.method == "post":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username,email,pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created.")
+
+        return redirect('/signin')
+
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "authentication/index.html", {'fname': fname})
+
+        else:
+            messages.error(request, "wrong Credentials")
+            return redirect('home')
+
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Your account has been successfully logged out.")
+    return redirect('home')
+
