3f74a943aa75324fd4a58cfcfc1d6a970ade9f7d
oli-mac <adumulugeta21@gmail.com>
2022-03-30 22:58:31 +0300

    project setup

---
+++ authentication/urls.py
@@ -0,0 +1,9 @@
+
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('',views.home, name="home"),
+]
---
+++ authentication/views.py
@@ -0,0 +1,8 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("hello bitch ass")
